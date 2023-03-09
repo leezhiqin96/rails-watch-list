be5203cf95f4f76ac5cd7354a006b86f9558f8d2
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-09 15:03:38 +0800

    Added create method for bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,4 +3,22 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:id, :movie_id, :list_id, :comment)
+  end
 end
