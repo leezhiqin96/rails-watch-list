7a385db4db389d0796524bf58a254ce779f53f2c
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-09 13:59:14 +0800

    Added list controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,17 @@
+class ListsController < ApplicationController
+  # GET lists/
+  def index
+  end
+
+  # GET lists/:id
+  def show
+  end
+
+  # GET lists/new
+  def new
+  end
+
+  # POST lists/
+  def create
+  end
+end
