96fac3182f4b64c9d396fda7a80349dcba0428d2
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-09 15:04:28 +0800

    Added destroy method for bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -16,6 +16,12 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def bookmark_params
