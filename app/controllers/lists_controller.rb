a23112f3fd8d73f6569f63413543804b28762e77
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-09 14:22:19 +0800

    Added new and create method for list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,15 +11,27 @@ class ListsController < ApplicationController
 
   # GET lists/new
   def new
+    @list = List.new
   end
 
   # POST lists/
   def create
+    @list = List.new(list_params)
+    @list.save
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
