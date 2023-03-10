a6c0fcaedb874d3b393819121e978a0d6b5047a0
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-10 15:12:39 +0800

    Include option for user to include image url when creating list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_081033) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_10_071029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_081033) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "movies", force: :cascade do |t|
