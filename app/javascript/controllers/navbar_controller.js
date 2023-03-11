37cd40722536f28411d72ebb2b7165b120ac9e17
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-11 10:18:48 +0800

    Deletes constant console.log

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
   updateNavbar() {
-    console.log(this);
     const threshold = 400;
     if (window.scrollY >= threshold) {
       this.element.classList.add("navbar-lewagon-grey")
